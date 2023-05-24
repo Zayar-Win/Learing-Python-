84c456b6fb46d84022adac73e8ebad02a9c34301
Zayar-Win <zayarwin7751@gmail.com>
2023-05-24 23:16:35 +0630

    python-user-input

---
+++ main.py
@@ -0,0 +1,37 @@
+first_name = "Zayar"
+last_name = " Win"
+age = 20
+
+name = first_name + last_name
+attractive = False
+
+role, senior, salary = "Developer", False, 0
+
+username = input("Enter your name : ")
+age = input("Enter your age : ")
+height = input("Enter your height : ")
+
+print("Your name is " + username)
+print('Your age is ' + age)
+print("Your height is " + height + "cm tall")
+
+# print(len(name))
+# print(name.count('Z'))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isalpha())
+# print(name.replace('Z', 'L'))
+# print(name*3)
+
+# print(role)
+# print(senior)
+# print(salary)
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# print('hello ', first_name + last_name)
+# print(type(first_name))
+# print(type(age))
