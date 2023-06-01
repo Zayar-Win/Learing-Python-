3d847e0ba07ec8fd371a7bd8ee51d9bd341ca847
Zayar-Win <zayarwin7751@gmail.com>
2023-06-02 00:53:08 +0630

    using-module

---
+++ module.py
@@ -0,0 +1,8 @@
+# module = a file containing python code . May contain functions,classes,etc...
+# used with modular programming,which is to sperate a program into parts
+# import messages
+# import messages as msg
+from messages import hello, bye
+
+hello()
+bye()
